b7c1b686a0bfcf5185ba54c1544d906e415ca777
Stu Bevan <stu.bevan@me.com>
2016-02-03 12:19:30 +0100

    Renamed

--- ping_drobofs.sh
+++ ping_drobofs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#
+# Script to help keep the DroboFS ssh daemon working
 
-exec >> "$(getlogfilename.sh)" 2>&1
-
-date
+. badger_setenv.sh $0
 
 export SSH_AUTH_SOCK=$( ls -1rt /tmp/launch*/Listeners | sed -n '$p' )
 
 ssh drobofs.badgers-place.me.uk /mnt/DroboFS/home/stu/bin/motherrang.sh /mnt/DroboFS/home/stu/.motherrang
+
+exit 0
 
