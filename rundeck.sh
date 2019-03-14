89effa494806a2b6b35031887f259da24452e667
Stu Bevan <stu@mees.st>
2019-03-14 16:33:07 +0100

    Tidying Up

--- rundeck.sh
+++ rundeck.sh
@@ -1,10 +1,17 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 
-#start rundeck
+# Setenv prog has to be in the same directory the script is run from
+rundir=$(dirname $0)
+. ${rundir}/badger_setenv.sh $0
 
-exec >> /usr/local/log/`date +%Y%m%d`-rundeck.log 2>&1
+
 cd /usr/local/rundeck
+<<<<<<< HEAD
 java -XX:MaxPermSize=1024M -Drundeck.jetty.connector.forwarded=true -Dserver.web.context=/ -Dserver.http.host=127.0.0.1 -jar rundeck-launcher-latest.jar
+=======
+export RDECK_BASE=/usr/local/rundeck
+java -Drundeck.ssl.config=$RDECK_BASE/server/config/ssl.properties -jar rundeck-launcher-latest.war
+>>>>>>> Initial-Code-Drop
 
 # We should never get here
 /usr/local/bin/pushover.sh -p 1 -t "System Alert" "MrBadger: Unexpected exit of rundeck"
