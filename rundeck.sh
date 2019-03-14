da4ec4ed2cdec21aa66f8895c3695dd6c3bed476
Stu Bevan <stu@mees.st>
2019-03-14 16:33:49 +0100

    Tidying Up

--- rundeck.sh
+++ rundeck.sh
@@ -6,12 +6,8 @@ rundir=$(dirname $0)
 
 
 cd /usr/local/rundeck
-<<<<<<< HEAD
-java -XX:MaxPermSize=1024M -Drundeck.jetty.connector.forwarded=true -Dserver.web.context=/ -Dserver.http.host=127.0.0.1 -jar rundeck-launcher-latest.jar
-=======
 export RDECK_BASE=/usr/local/rundeck
 java -Drundeck.ssl.config=$RDECK_BASE/server/config/ssl.properties -jar rundeck-launcher-latest.war
->>>>>>> Initial-Code-Drop
 
 # We should never get here
 /usr/local/bin/pushover.sh -p 1 -t "System Alert" "MrBadger: Unexpected exit of rundeck"
